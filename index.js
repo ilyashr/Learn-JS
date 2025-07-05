673f4e3a3a52a0887d3f71bbd87c2ac765fd1020
ILYA SCHERBATYH <ilsherbatyh@gmail.com>
2025-07-05 23:51:27 +0300

    Century of year

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ console.log(toPower(2,3));
 
 const toPowerArrow = (num, power) => num ** power;
 console.log(toPowerArrow(2,3));
+
+// 5 https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097 
+//Century From Year
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task: Given a year, return the century it is in.
+// Examples 1705 --> 18 1900 --> 19 1601 --> 17 2000 --> 20 2742 --> 28
+
+const century = year => Math.ceil(year / 100);
