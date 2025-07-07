d00b070d8cbdfe25b487339afc61289b099bef47
ilyashr <ilsherbatyh@gmail.com>
2025-07-08 00:42:52 +0300

    Покупка MacBook

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-/* Требуется переписать функцию из обычной в стрелочную */
-
-function toPower(num, power) {
-	const res = num ** power;
-	return res;
-}
-
-console.log(toPower(2,3));
-
-/* Стрелочная функция */
-
-const toPowerArrow = (num, power) => num ** power;
-console.log(toPowerArrow(2,3));
+// Задачи CodeWars
 
 // 1. https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b
 // Cat years, Dog years
@@ -29,3 +17,39 @@ function humanYearsCatYearsDogYears (humanYears, catYears, dogYears) {
 //Century From Year
 
 const century = year => Math.ceil(year / 100);
+
+// Задачи по основам JavaScript:
+
+// Требуется переписать функцию из обычной в стрелочную
+
+function toPower(num, power) {
+	const res = num ** power;
+	return res;
+}
+
+console.log(toPower(2,3));
+
+// Стрелочная функция
+
+const toPowerArrow = (num, power) => num ** power;
+console.log(toPowerArrow(2,3));
+
+// Упражнение Ларичева. Покупка MacBook
+
+function computeCredit (age, hasJob) {
+	switch(true) {
+		case age > 24 && hasJob:
+			return 500;
+		case age > 24:
+			return 100;
+		default:
+			return 0;
+}
+}
+console.log(computeCredit(25, true))
+
+function canBuy (productPrice, age, money, hasJob = true) {
+	const creditMoney = computeCredit(age, hasJob);
+	return productPrice <= money + creditMoney;
+}
+console.log(canBuy(2000, 25, 1500))
