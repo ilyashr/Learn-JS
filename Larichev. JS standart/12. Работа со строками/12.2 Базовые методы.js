0d1200cf1bb78f4af3ef4c66dc050a5fc4f6816c
ilyashr <ilsherbatyh@gmail.com>
2025-07-18 23:19:21 +0300

    Строки. Базовые методы

---
+++ Larichev. JS standart/12. Работа со строками/12.2 Базовые методы.js
@@ -0,0 +1,11 @@
+const userName = 'Вася Пупкин';
+console.log(userName[0]);
+console.log(userName[1]);
+console.log(userName[2]);
+console.log(userName.charAt(2));
+console.log(userName.length);
+console.log(userName.indexOf('я'));
+console.log(userName.lastIndexOf('П'));
+console.log(userName.includes('П'));
+console.log(userName.slice(5));
+console.log(userName.slice(5, 8));
