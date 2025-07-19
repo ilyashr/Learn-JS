c3f2a2dc911c096af0e27ac1248e364fcf33b8c5
ilyashr <ilsherbatyh@gmail.com>
2025-07-19 23:29:42 +0300

    Выделение имени

---
+++ Larichev. JS standart/12. Работа со строками/Упражнение - выделение имени.js
@@ -0,0 +1,20 @@
+//вытащить имя и фамилию в отдельные переменные
+
+// мое решение
+const userName = 'Вася aka Terminator Пупкин';
+const firstName  = (userName.slice(20))
+console.log(firstName);
+const lastName  = (userName.slice(0, 5))
+console.log(lastName);
+
+// решение Ларичева
+
+const fullUserName1 = 'Вася aka Terminator Пупкин';
+
+const userName1 = fullUserName1.slice(0, fullUserName1.indexOf(' '));
+console.log(userName1);
+const userSurname = fullUserName1.slice(
+    fullUserName1.lastIndexOf(' ') + 1,
+    fullUserName1.length
+)
+console.log(userSurname);
