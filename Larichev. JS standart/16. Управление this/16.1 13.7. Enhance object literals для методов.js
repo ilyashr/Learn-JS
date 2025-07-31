100ce9125df0366b0b51802f29dbd64af13ebeec
ilyashr <ilsherbatyh@gmail.com>
2025-07-31 13:27:50 +0300

    Call, apply

---
+++ Larichev. JS standart/16. Управление this/16.1 13.7. Enhance object literals для методов.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const b =1;
+const a = {
+    b,
+    getB: function () {
+        return this.b;
+    },
+    getBAlt() {
+        return this.b;
+    }
+}
+console.log(a.getBAlt());
---
+++ Larichev. JS standart/16. Управление this/16.2 Call, apply.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const audi = {
+    make: 'Audi',
+    model: 'A3',
+    year: 2021,
+    damages: [],
+    addDamage(part, rate) {
+        console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено повреждение ${part} со степенью ${rate}`
+        )
+        this.damages.push({
+            part,
+            rate
+        })
+    }
+}
+audi.addDamage('Капот', 1);
+
+const bmw = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2022,
+    damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2);
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2);
+addDamageFunc.call(bmw, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+addDamageFunc.apply(audi, ['Бампер', 2]);
+// пример со spread оператором
+addDamageFunc.call(bmw, ...['Бампер', 2]);
