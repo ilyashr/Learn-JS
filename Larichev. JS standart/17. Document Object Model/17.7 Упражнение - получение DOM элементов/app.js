632ff38122ff89a271c9ce375a18b3671f08fcba
ilyashr <ilsherbatyh@gmail.com>
2025-08-02 18:50:51 +0300

    Dom elements 2

--- Larichev. JS standart/17. Document Object Model/17.7 Упражнение - получение DOM элементов/app.js
+++ Larichev. JS standart/17. Document Object Model/17.7 Упражнение - получение DOM элементов/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-console.log(document.querySelector('.one').innerText);
-console.log(document.querySelector('.one ~ div').innerText);
+//console.log(document.querySelector('.one').innerText);
+//console.log(document.querySelector('.one ~ div').innerText);
+
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+
+console.log(document.getElementsByClassName('one'));
+
 console.log(document.querySelector('#two').innerText);
+
+console.log(document.getElementById('two').innerText);
+
 console.log(document.querySelector('[user-id="4"]').innerText);
+
