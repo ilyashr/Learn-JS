5177bef33563698553c1060e78e8f7175651b25c
ilyashr <ilsherbatyh@gmail.com>
2025-09-12 22:26:37 +0300

    Flick Switch

--- Codewars/Flick Switch.js
+++ Codewars/Flick Switch.js
@@ -1,3 +1,15 @@
 // Flick Switch
 // https://www.codewars.com/kata/64fbfe2618692c2018ebbddb
 
+function flickSwitch(arr){
+  let result = [];
+  let currentState = true;
+  
+  for (let item of arr) {
+    if (item === 'flick') {
+      currentState = !currentState;
+    }
+  result.push(currentState);
+    }
+  return result;
+}
