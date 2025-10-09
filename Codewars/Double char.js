e060671f3d6af584772bc16cd2d61a7633c4365f
ilyashr <ilsherbatyh@gmail.com>
2025-10-09 22:31:36 +0300

    Add descriptions

--- Codewars/Double char.js
+++ Codewars/Double char.js
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/56b1f01c247c01db92000076
 // Double char
 
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output): "String"      -> "SSttrriinngg", "1234!_ "     -> "11223344!!__  "
+
 // Для решения задачи написал себе пример с выводом результата в консоль
 let str = 'string!';
 // Преобразование строки в массив
--- Codewars/Flick Switch.js
+++ Codewars/Flick Switch.js
@@ -1,6 +1,14 @@
 // Flick Switch
 // https://www.codewars.com/kata/64fbfe2618692c2018ebbddb
 
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+// Notes
+// "flick" will always be given in lowercase.
+// A list may contain multiple flicks.
+// Switch the boolean value on the same element as the flick itself.
+
 function flickSwitch(arr){
   let result = [];
   let currentState = true;
